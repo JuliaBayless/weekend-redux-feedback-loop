7d71ede7daa594aafb9207114606d0bb24570246
Julia Bayless <julia.allistair@gmail.com>
2021-10-30 13:05:37 -0500

    feedback reducer done with dummy data for object

--- src/index.js
+++ src/index.js
@@ -3,6 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const feedBackDummyData = {
+    feeling: 0,
+    understanding: 0,
+    support: 0,
+    comments: '',
+    flagged: false,
+    date: ''
+}
+//reducer thats holding all the info from feedback
+const feedbackReducer = (state = feedBackDummyData, action) => {
+    if(action.type === "SUBMIT_FEELING") {
+        return{ ...state, feeling: action.payload};
+    } else if (action.type === "SUBMIT_UNDERSTANDING") {
+        return{ ...state, understanding: action.payload};
+    } else if (action.type === "SUBMIT_SUPPORT") {
+        return{ ...state, support : action.payload};
+    } else if (action.type === "SUBMIT_COMMENTS") {
+        return{ ...state, comments : action.payload};
+    }
+    return state
+};
+
+
+
+ReactDOM.render(
+
+<Provider>
+<App />
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
