4fc9e0d27f071aae781d94e1995aaeee05491580
Julia Bayless <julia.allistair@gmail.com>
2021-10-30 14:12:10 -0500

    support page complete -> comments next

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -1,8 +1,41 @@
-
+import { useHistory } from "react-router";
+import { useDispatch } from "react-redux"
+import { useState } from "react";
 
 function Support() {
+    const [support, setSupport] = useState('');
+    //navigate to the next page with history
+    const history = useHistory();
+    // trigger that sends data to reducer
+    const dispatch = useDispatch();
 
 
+    //function that will dispatch data to reducer 
+    //and use history
+    const handleSubmitSupport = () => {
+        event.preventDefault();
+        //send info off with trigger
+        dispatch({
+            type: 'SUBMIT_SUPPORT',
+            payload: support
+        });
+        history.push('/comments')
+    }
+
+    return (
+        <>
+            <h1>How supported do you feel?</h1>
+            <p>Please rate between a 0-5</p>
+            <div className="inputBox">
+                <input type="number"
+                    // value={feeling} -> don't need this if I want data to stay for 'back' button
+                    onChange={(event) => setSupport(event.target.value)}
+                />
+                <button className="Submit"
+                    onClick={handleSubmitSupport}>Next</button>
+            </div>
+        </>
+    )
 }//end Support
 
 export default Support;
--- src/components/Understanding/Understanding.jsx
+++ src/components/Understanding/Understanding.jsx
@@ -1,7 +1,41 @@
-
+import { useHistory } from "react-router";
+import { useDispatch } from "react-redux"
+import { useState } from "react";
 
 function Understanding() {
+    const [understanding, setUnderstanding] = useState('');
+    //navigate to the next page with history
+    const history = useHistory();
+    // trigger that sends data to reducer
+    const dispatch = useDispatch();
 
+
+    //function that will dispatch data to reducer 
+    //and use history
+    const handleSubmitUnderstanding = () => {
+        event.preventDefault();
+        //send info off with trigger
+        dispatch({
+            type: 'SUBMIT_UNDERSTANDING',
+            payload: understanding
+        });
+        history.push('/support')
+    }
+
+    return (
+        <>
+            <h1>How do you feel you are understanding the material?</h1>
+            <p>Please rate between a 0-5</p>
+            <div className="inputBox">
+                <input type="number"
+                    // value={feeling} -> don't need this if I want data to stay for 'back' button
+                    onChange={(event) => setUnderstanding(event.target.value)}
+                />
+                <button className="Submit"
+                    onClick={handleSubmitUnderstanding}>Next</button>
+            </div>
+        </>
+    )
 
 }//end Understanding
 
