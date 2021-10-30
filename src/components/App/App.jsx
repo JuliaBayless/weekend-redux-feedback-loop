8ad89fb8e545abac45fc263c1b58ba267b14cac9
Julia Bayless <julia.allistair@gmail.com>
2021-10-30 14:06:16 -0500

    feeling page done with history.push to understanding

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,12 @@ import axios from 'axios';
 import './App.css';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import Admin from '../Admin/Admin';
+import Feelings from '../Feelings/Feelings';
+import Understanding from '../Understanding/Understanding';
+import Support from '../Support/Support';
+import Comments from '../Comments/Comments';
+import ThankYou from '../ThankYou/ThankYou';
+import Review from '../Review/Review';
 
 function App() {
 
@@ -13,6 +19,26 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
         </header>
+        <Route exact path="/">
+          <Feelings />
+        </Route>
+        <Route exact path="/understanding">
+          <Understanding />
+        </Route>
+        <Route exact path="/support">
+          <Support />
+        </Route>
+        <Route exact path="/comments">
+          <Comments />
+        </Route>
+        <Route exact path="/review">
+          <Review />
+        </Route>
+        <Route exact path="/ThankYou">
+          <ThankYou />
+        </Route>
+
+      {/* Path to Admin page */}
         <Route exact path="/admin">
           <Admin />
         </Route>
--- src/components/Feelings/Feelings.jsx
+++ src/components/Feelings/Feelings.jsx
@@ -1,7 +1,41 @@
-
+import { useHistory } from "react-router";
+import { useDispatch } from "react-redux"
+import { useState } from "react";
 
 function Feelings() {
+    const [feeling, setFeeling] = useState('');
+    //navigate to the next page with history
+    const history = useHistory();
+    // trigger that sends data to reducer
+    const dispatch = useDispatch();
 
+
+    //function that will dispatch data to reducer 
+    //and use history
+    const handleSubmitFeelings = () => {
+        event.preventDefault();
+        //send info off with trigger
+        dispatch({
+            type: 'SUBMIT_FEELING',
+            payload: feeling
+        });
+        history.push('/understanding')
+    }
+
+return (
+    <>
+    <h1>How are you feeling today?</h1>
+    <p>Please rate between a 0-5</p>
+    <div className="inputBox">
+        <input type="number" 
+        // value={feeling} -> don't need this if I want data to stay for 'back' button
+        onChange={(event) => setFeeling(event.target.value)}
+        />
+        <button className="Submit" 
+        onClick={handleSubmitFeelings}>Next</button>
+    </div>
+    </>
+)
 
 }//end Feelings
 
